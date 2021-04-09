3f767ae76ac7bb0326e84e4497a38bed85d907e4
Feimei Chen <feimei@hawaii.edu>
2021-04-09 13:45:24 -1000

    landing page

---
+++ app/imports/ui/components/LandingTop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Grid, Header} from 'semantic-ui-react';
+// import { Grid, Image } from 'semantic-ui-react';
+
+/** A simple static component to render some text for the landing page. */
+class LandingTop extends React.Component {
+  render() {
+    return (
+      <div className ="landing-middle">
+        <Grid container centered>
+          <Header as="h1" inverted>Welcome!</Header>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default LandingTop;
