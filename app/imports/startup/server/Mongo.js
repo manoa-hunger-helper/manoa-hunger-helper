3920b8b25bfc98a50edcfe9a0dc8792d4a4d44b3
sulao1999 <sulao@hawaii.edu>
2021-04-10 03:02:39 -1000

    vendors database

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Vendors } from '../../api/vendor/Vendor.js';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,24 @@ function addData(data) {
   Stuffs.collection.insert(data);
 }
 
+// Initialize the database with a default vendor document.
+function addVendor(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Vendors.collection.insert(data);
+}
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the VendorsCollection if empty.
+if (Vendors.collection.find().count() === 0) {
+  if (Meteor.settings.defaultVendor) {
+    console.log('Creating default vendor.');
+    Meteor.settings.defaultVendor.map(data => addVendor(data));
   }
 }
