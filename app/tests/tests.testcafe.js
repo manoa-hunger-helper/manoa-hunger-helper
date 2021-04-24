383c5e18e1f3014d8e79fb55ac346c5d6547b341
sulao1999 <sulao@hawaii.edu>
2021-04-23 17:35:05 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -69,7 +69,7 @@ test('Test the Today Top Picks page', async (testController) => {
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoTodayTopPicksPage(testController);
   await todayTopPicksPage.isDisplayed(testController);
-  // await todayTopPicksPage.hasCard(testController);
+  await todayTopPicksPage.hasCard(testController);
 });
 
 test('Test the Menu page', async (testController) => {
