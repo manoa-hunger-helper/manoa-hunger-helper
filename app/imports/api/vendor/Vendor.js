da6e054aa317b70d5c8f788b39b19e9544317bea
Jessica Ocampo <jnocampo@hawaii.edu>
2021-04-25 21:38:38 -1000

    Adding description to vendor cards

--- app/imports/api/vendor/Vendor.js
+++ app/imports/api/vendor/Vendor.js
@@ -48,6 +48,7 @@ class VendorCollection {
       email: String,
       type: String,
       owner: String,
+      description: String,
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
