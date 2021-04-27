1ab3886e4885e0f8e40a6cd45d8b95e0a7311274
Feimei Chen <feimei@hawaii.edu>
2021-04-27 10:00:25 -1000

    fix all vendor page

--- app/imports/ui/components/VendorItem.jsx
+++ app/imports/ui/components/VendorItem.jsx
@@ -22,12 +22,12 @@ class VendorItem extends React.Component {
     const { activeIndex } = this.state;
     return (
       <Card>
-        <Image size='large' src={vendorInfo.image} wrapped ui={false}/>
+        <Image size='large' src={vendorInfo.image} wrapped ui={false} style={{ height: '290px' }}/>
         <Card.Content>
           <Card.Header>{vendorInfo.name}</Card.Header>
           <Card.Meta>{vendorInfo.location}</Card.Meta>
-          <Card.Meta>{vendorInfo.operationDays}</Card.Meta>
-          <Card.Meta>{vendorInfo.operationHours}</Card.Meta>
+          <Card.Meta>{vendorInfo.businessdate}</Card.Meta>
+          <Card.Meta>From {vendorInfo.starttime}:00 to {vendorInfo.endtime}:00</Card.Meta>
           <Card.Meta>{vendorInfo.paymentOptions}</Card.Meta>
           <Card.Meta>
             <Accordion>
@@ -36,36 +36,13 @@ class VendorItem extends React.Component {
                 index={0}
                 onClick={this.handleClick}>
                 <Icon name='dropdown'/>
-                Click to view dietary options
+                Click to view my websit
               </Accordion.Title>
               <Accordion.Content active={activeIndex === 0}>
-                {vendorInfo.email}
+                <a href={vendorInfo.website}> {vendorInfo.website}</a>
               </Accordion.Content>
             </Accordion>
           </Card.Meta>
-          <Card.Meta>
-            {this.props.vendor.address}
-          </Card.Meta>
-          <Card.Meta>
-            {this.props.vendor.phone}
-          </Card.Meta>
-          <Card.Description>
-            <Accordion>
-              <Accordion.Title
-                acive={activeIndex === 0}
-                index={0}
-                onClick={this.handleClick}>
-                <Icon name='dropdown'/>
-                Click to see description
-              </Accordion.Title>
-              <Accordion.Content active={activeIndex === 0}>
-                {vendorInfo.description}
-              </Accordion.Content>
-            </Accordion>
-          </Card.Description>
-          {Roles.userIsInRole(Meteor.userId(), 'vendor') ? (<Card.Content extra>
-            <Link to={`/edit/${this.props.vendor._id}`}>Edit</Link>
-          </Card.Content>) : ''}
         </Card.Content>
         <Card.Content extra>
           <Link to={'/pick'}><Button color={'red'}>Find My Menu</Button></Link>
